7da8a11f5fafeb1eec33d1b99e504c1cb2ceb357
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-05-02 15:12:21 -0300

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  entry: { index: path.resolve(__dirname, 'src', 'index.js') },
   mode: 'development',
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
       },
       {
-        type: "asset",
-        test: /\.(png|svg|jpg|jpeg|gif)$/i, 
-      }
-    ]
+        type: 'asset',
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html")
-    })
-  ]
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
 };
