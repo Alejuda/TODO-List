16db3a10be2d13895e8b2ab45e2476da275c1b53
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-05-03 14:08:13 -0300

    Add CRUD to the task list

---
+++ src/toggleComplete.js
@@ -0,0 +1,18 @@
+import renderList from "./renderList.js";
+
+const toggleComplete = (id) => {
+  let list = JSON.parse(localStorage.getItem("tasks-list"));
+  if (list === null) {
+    list = [];
+  }
+  const toggle = list.findIndex((task) => task.id === id);
+  if (!list[toggle].completed) {
+    list[toggle].completed = true;
+  } else {
+    list[toggle].completed = false;
+  }
+  localStorage.setItem("tasks-list", JSON.stringify(list));
+  renderList();
+};
+
+export default toggleComplete;
