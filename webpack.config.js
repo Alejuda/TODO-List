42322166716d29c61514201df2b58466735ab919
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-05-03 20:09:06 -0300

    Change dist folder name for /docs to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 module.exports = {
   entry: { index: path.resolve(__dirname, 'src', 'index.js') },
+  output: {
+    path: path.resolve(__dirname, "docs")
+  },
   mode: 'development',
   module: {
     rules: [
