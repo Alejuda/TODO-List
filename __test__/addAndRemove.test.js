afeb42f88c2b6fe28893b445ad86b894e4a5f262
lincoln gibson <lincolngibson7@gmail.com>
2023-05-10 19:58:11 -0300

    Add test to remove item for localstorage.

--- __test__/addAndRemove.test.js
+++ __test__/addAndRemove.test.js
@@ -1,10 +1,10 @@
-import { addTask, removeTask } from "./functionTests.js";
+import { addTask, removeTask } from './functionTests.js';
 
 const localStorageMock = (() => {
   let store = {};
 
   return {
-    store: store,
+    store,
     getItem: ((key) => store[key] || []),
     setItem: ((key, value) => {
       store[key] = JSON.stringify(value);
@@ -20,38 +20,38 @@ const localStorageMock = (() => {
 
 global.localStorage = localStorageMock;
 
-describe("addTask", () => {
-  it("should add a new task to the list in localStorage", () => {
-    const mockDescription = "New task";
+describe('addTask', () => {
+  it('should add a new task to the list in localStorage', () => {
+    const mockDescription = 'New task';
     const expectedList = [
       {
         id: 1,
-        desc: "New task",
+        desc: 'New task',
         completed: false,
       },
     ];
 
     addTask(mockDescription);
-    expect(JSON.parse(localStorage.store["tasks-list"])).toStrictEqual(
-      expectedList
+    expect(JSON.parse(localStorage.store['tasks-list'])).toStrictEqual(
+      expectedList,
     );
   });
 });
 
-describe("removeTask", () => {
-  it("should remove a task from the list in localStorage", () => {
+describe('removeTask', () => {
+  it('should remove a task from the list in localStorage', () => {
     const removeId = 1;
     const removeList = [
       {
         id: 1,
-        desc: "New task",
+        desc: 'New task',
         completed: false,
       },
     ];
 
-    addTask(mockDescription);
-    expect(JSON.parse(localStorage.store["tasks-list"])).toStrictEqual(
-      expectedList
-    );
+    localStorage.setItem('tasks-list', removeList);
+
+    removeTask(removeId);
+    expect(JSON.parse(localStorage.store['tasks-list'])).toStrictEqual('[]');
   });
 });
--- __test__/functionTests.js
+++ __test__/functionTests.js
@@ -9,11 +9,11 @@ export const addTask = (desc) => {
     completed: false,
   };
   list.push(newTask);
-  localStorage.setItem("tasks-list", list);
+  localStorage.setItem('tasks-list', list);
 };
 
 export const removeTask = (id) => {
-  let list = JSON.parse(localStorage.getItem("tasks-list"));
+  let list = JSON.parse(localStorage.getItem('tasks-list'));
   if (list === null) {
     list = [];
   }
@@ -22,5 +22,5 @@ export const removeTask = (id) => {
   for (let i = id - 1; i < list.length; i += 1) {
     list[i].id = i + 1;
   }
-  localStorage.setItem("tasks-list", JSON.stringify(list));
+  localStorage.setItem('tasks-list', JSON.stringify(list));
 };
