a2db55c7c6f665594a3941dd29aefdca9aa76980
lincoln gibson <lincolngibson7@gmail.com>
2023-05-11 14:20:04 -0300

    Add describe function to wrap completed task tests.

--- __test__/addAndRemove.test.js
+++ __test__/addAndRemove.test.js
@@ -1,6 +1,12 @@
-import { JSDOM } from 'jsdom';
 import {
-  addTask, removeTask, toggleComplete, deleteAllCompleted, editTask,
+  JSDOM,
+} from 'jsdom';
+import {
+  addTask,
+  removeTask,
+  toggleComplete,
+  deleteAllCompleted,
+  editTask,
 } from './functionTests.js';
 
 const dom = new JSDOM('<!doctype html><html><body></body></html>');
@@ -10,13 +16,11 @@ global.document = window.document;
 describe('addTask', () => {
   it('should add a new task to the list in localStorage', () => {
     const mockDescription = 'New task';
-    const expectedList = [
-      {
-        id: 1,
-        desc: 'New task',
-        completed: false,
-      },
-    ];
+    const expectedList = [{
+      id: 1,
+      desc: 'New task',
+      completed: false,
+    }];
 
     addTask(mockDescription);
     expect(JSON.parse(localStorage.store['tasks-list'])).toStrictEqual(
@@ -28,91 +32,16 @@ describe('addTask', () => {
 describe('removeTask', () => {
   it('should remove a task from the list in localStorage', () => {
     const removeId = 1;
-    const removeList = [
-      {
-        id: 1,
-        desc: 'New task',
-        completed: false,
-      },
-    ];
+    const removeList = [{
+      id: 1,
+      desc: 'New task',
+      completed: false,
+    }];
 
     localStorage.setItem('tasks-list', removeList);
 
     removeTask(removeId);
     expect(JSON.parse(localStorage.store['tasks-list'])).toStrictEqual('[]');
-  });
-});
-
-describe('toggleCmplete', () => {
-  it('should toggle completed attribute of a task in the localStorage', () => {
-    const toggleId = 1;
-    const toggleList = [
-      {
-        id: 1,
-        desc: 'New task',
-        completed: false,
-      },
-    ];
-    const localExpected = [
-      {
-        id: 1,
-        desc: 'New task',
-        completed: true,
-      },
-    ];
-
-    localStorage.setItem('tasks-list', toggleList);
-
-    toggleComplete(toggleId);
-    expect(JSON.parse(localStorage.store['tasks-list'])).toStrictEqual(localExpected);
-  });
-});
-
-describe('removeCompleted task', () => {
-  it('should remove the task marked as completed', () => {
-    const deleteContents = [
-      {
-        id: 1,
-        desc: 'New task',
-        completed: false,
-      },
-      {
-        id: 2,
-        desc: 'New task',
-        completed: true,
-      },
-    ];
-    const localExpected = [
-      {
-        id: 1,
-        desc: 'New task',
-        completed: false,
-      },
-    ];
-
-    localStorage.setItem('tasks-list', deleteContents);
-
-    deleteAllCompleted(deleteContents);
-    expect(JSON.parse(localStorage.store['tasks-list'])).toStrictEqual(localExpected);
-  });
-  it('should not remove the task not marked as completed', () => {
-    const deleteContents = [
-      {
-        id: 1,
-        desc: 'New task',
-        completed: false,
-      },
-      {
-        id: 2,
-        desc: 'New task',
-        completed: false,
-      },
-    ];
-
-    localStorage.setItem('tasks-list', deleteContents);
-
-    deleteAllCompleted(deleteContents);
-    expect(JSON.parse(localStorage.store['tasks-list'])).toStrictEqual(deleteContents);
   });
 });
 
@@ -129,16 +58,77 @@ describe('editTask', () => {
   });
 
   it('should edit the description of the task with the parameter id', () => {
-    const deleteContents = [
-      {
-        id: 1,
-        desc: 'EDIT TASK',
-        completed: false,
-      },
-    ];
+    const deleteContents = [{
+      id: 1,
+      desc: 'EDIT TASK',
+      completed: false,
+    }];
 
     localStorage.setItem('tasks-list', deleteContents);
 
     editTask(1);
+  });
+});
+
+describe('Operations on Completed tasks', () => {
+  describe('toggleCmplete', () => {
+    it('should toggle completed attribute of a task in the localStorage', () => {
+      const toggleId = 1;
+      const toggleList = [{
+        id: 1,
+        desc: 'New task',
+        completed: false,
+      }];
+      const localExpected = [{
+        id: 1,
+        desc: 'New task',
+        completed: true,
+      }];
+
+      localStorage.setItem('tasks-list', toggleList);
+
+      toggleComplete(toggleId);
+      expect(JSON.parse(localStorage.store['tasks-list'])).toStrictEqual(localExpected);
+    });
+  });
+
+  describe('removeCompleted task', () => {
+    it('should remove the task marked as completed', () => {
+      const deleteContents = [{
+        id: 1,
+        desc: 'New task',
+        completed: false,
+      }, {
+        id: 2,
+        desc: 'New task',
+        completed: true,
+      }];
+      const localExpected = [{
+        id: 1,
+        desc: 'New task',
+        completed: false,
+      }];
+
+      localStorage.setItem('tasks-list', deleteContents);
+
+      deleteAllCompleted(deleteContents);
+      expect(JSON.parse(localStorage.store['tasks-list'])).toStrictEqual(localExpected);
+    });
+    it('should not remove the task not marked as completed', () => {
+      const deleteContents = [{
+        id: 1,
+        desc: 'New task',
+        completed: false,
+      }, {
+        id: 2,
+        desc: 'New task',
+        completed: false,
+      }];
+
+      localStorage.setItem('tasks-list', deleteContents);
+
+      deleteAllCompleted(deleteContents);
+      expect(JSON.parse(localStorage.store['tasks-list'])).toStrictEqual(deleteContents);
+    });
   });
 });
